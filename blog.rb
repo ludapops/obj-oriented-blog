982254e720a16c830cfddb2e777ca9a325105982
Marc Mathieu <goheat631@gmail.com>
2015-01-13 16:30:02 -0500

    addedblog.rb

---
+++ blog.rb
@@ -0,0 +1,16 @@
+class Blog 
+	postprint ="************"
+	def initialie()
+	end
+
+	def frontPage
+		puts "This is Marc's Blizzog"
+	end
+end
+
+class Post
+
+end
+
+blog = Blog.new
+blog.frontPage
